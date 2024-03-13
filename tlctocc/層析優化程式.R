4807f0811104058aa81f8df857fcd1f1af3861f8
Shuang-Yu Sun <162078691+Shuangyu-Casper@users.noreply.github.com>
2024-03-13 16:56:18 +0800

    Add files via upload

--- tlctocc/層析優化程式.R
+++ tlctocc/層析優化程式.R
@@ -3,6 +3,7 @@ VMpred <- function (x){
   1.8*x+0.3
 }
 
+
 # 階段沖堤的預測模型
 PRED <- function(x){
   VR <- c(0, 0, 0)
@@ -10,6 +11,7 @@ PRED <- function(x){
   P <- c(rep(1,length(VE)))
   ER <- c(rep(0,length(VE)))
   state <- c(rep("已洗脫",length(VE)))
+  RM <- log10(1/x-1)
   
   for (i in 1:length(VE)){
     VRadj[i] <- P[i] * Ktr * (1/x[i]-1) * VM
@@ -28,30 +30,31 @@ PRED <- function(x){
     }
     
     if(ER[i] == 1){
-      state[i]<-"已洗脫"
+      state[i]<-"已沖提出"
     }else{
-      state[i]<-"未洗脫"
+      state[i]<-"未沖提出"
     }
   }
-  data.frame(x, x*RFadjF, VR, VRadj, state)
+  data.frame(x, x*RFadjF, RM, VR, VRadj, state)
 }
 
-VRTotal <- function(x){
+VEPred <- function(x){
   e=0
-  VRtotal <- 0 
+  VEPred <- 0 
   for(i in 1:length(VE)){
-    if(x$state[i]=="已洗脫"){
+    if(x$state[i]=="已沖提出"){
       e <- i
       break
     }
   }
   if(e>1){
     for(i in 1:(e-1)){
-      VRtotal <- VRtotal + VE[i]
+      if(is.finite(VE[i])){
+        VEPred <- VEPred + VE[i]}
     }
   }
-  VRtotal <- VRtotal + x$VR[e]
-  return(VRtotal)
+  VEPred <- VEPred + x$VR[e]
+  return(VEPred)
 }
 
 Vb <- function(x, N){
@@ -63,44 +66,40 @@ filepath <- readline("請輸入檔案：")
 DATA <- read.csv(filepath)
 
 
-# 設定矽膠重、死體積和理論板數
-W <- 40
-VM <- VMpred(10)
-N <- 90
+# 設定死體積和理論板數
+VM <- VMpred(24)
+N <- 180
 
 # 指定校正RF值時使用的倍率和Ktr值
 Ktr <- 1
 RFadjF <- 1.5
 
 # 輸入各沖堤體積和對應的RF值
-VE <- c(120, 120, 120)
-RFA <- c(0.2, 0.3, 0.4)
-RFB <- c(0.1, 0.1, 0.2)
-
-# 計算校正後RF值
-RFAadj <- RFA*RFadjF
-RFBadj <- RFB*RFadjF
+VE <- c(90, 80, 75, 90, 80, 75, 84, 90)
+RFA <- c(0, 0.15, 0.23, 0.26, 0.45, 0.45, 0.47, 0.50)
+RFB <- c(0, 0.13, 0.17, 0.20, 0.37, 0.37, 0.43, 0.45)
 
 # 預測A化合物滯留體積
 compoundA <- PRED(RFA)
-VRTotalA <- VRTotal(compoundA)
-VbA <- Vb(VRTotalA, N)
+VEPredA <- VEPred(compoundA)
+VbA <- Vb(VEPredA, N)
 
 # 預測B化合物滯留體積
 compoundB <- PRED(RFB)
-VRTotalB <- VRTotal(compoundB)
-VbB <- Vb(VRTotalB, N)
+VEPredB <- VEPred(compoundB)
+VbB <- Vb(VEPredB, N)
 
 
 result <- data.frame(VE, compoundA, compoundB)
-colnames(result) <- c("VE", "RF(A)", "RF'(A)", "VR(A)", "VR'(A)", "狀態(A)",
-                      "RF(B)", "RF'(B)", "VR(B)", "VR'(B)", "狀態(B)")
+colnames(result) <- c("VE", "RF(A)", "RF'(A)","RM(A)", "VR(A)", "VR'(A)", "狀態(A)",
+                      "RF(B)", "RF'(B)","RM(B)", "VR(B)", "VR'(B)", "狀態(B)")
 
 
-Rs <- abs(2*(VRTotalB-VRTotalA)/(VbA+VbB))
+Rs <- abs(2*(VEPredB-VEPredA)/(VbA+VbB))
 
-summary <- rbind(VR(A), Vb(A), VR(B), Vb(B), Rs)
+summary <- rbind(VEPredA, VbA, VEPredB, VbB, Rs)
 sumaary <- data.frame(summary)
+
 
 # 繪製示意圖
 # 請使用者輸入圖檔儲存路徑
@@ -108,14 +107,14 @@ filepath <- readline("請輸入儲存結果的檔案路徑：")
 setwd(filepath)
 png("chromotagram.png", width = 1200, height = 900, res=300)
 par(mar=c(4, 0.5, 0.5, 0.5))
-x <- seq(0, VRTotalB+VbB+10, by = 0.333)
-yA <- dnorm(x, mean = VRTotalA, sd = VbA/4)
-yB <- dnorm(x, mean = VRTotalB, sd = VbB/4)
+x <- seq(0, VEPredB+VbB+10, by = 0.333)
+yA <- dnorm(x, mean = VEPredA, sd = VbA/4)
+yB <- dnorm(x, mean = VEPredB, sd = VbB/4)
 plot(x, yA, type="l", xlab="沖堤體積 (mL)", ylab="", yaxt="n")
 par(new=TRUE)
 plot(x, yB, type="l", xlab="沖堤體積 (mL)", ylab="", yaxt="n")
 dev.off()
 
 # 匯出CSV檔案
-write.csv(result, file="result.csv",row.names = T,  fileEncoding = "Big5")
-write.csv(summary, file="summary.csv",row.names = T,  fileEncoding = "UTF-8")
+write.csv(result, file="result.csv", row.names = T,  fileEncoding = "Big5")
+write.csv(summary, file="summary.csv", row.names = T,  fileEncoding = "Big5")
